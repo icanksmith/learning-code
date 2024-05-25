b5cfaa1b388d5314547b12d0de805053edd57806
icank smith <sammykitkat8@gmail.com>
2024-05-25 13:49:07 +0700

    test

---
+++ basic1.js
@@ -0,0 +1,30 @@
+
+//learn basic js
+
+let a="b";
+let b="a";
+const c= a+b;
+console.log(c);
+
+let d=10;
+let e=5;
+const f=d+e;
+console.log(f)
+
+let myDataId = "ini data saya";
+
+console.log(myDataId);
+
+myDataId = "ini saya";
+
+console.log(myDataId);
+
+// let bisa diubah kapan saja 
+// const hanya bisa digunakan sekali
+
+const data1 = "samuel fransisco jago ngoding";
+
+// apabila diganti setelah ini maka akan terjadi error
+// data1 = "-" < error code
+
+console.log(data1);
---
+++ testing.js
@@ -0,0 +1,20 @@
+
+console.log("Hello World!");
+
+//Belajar membuat algoritma deskriptif
+
+/*
+Cara memakan permen lolipop 
+misalnya mulut kita hanya muat untuk memakan 1 buah permen lolipop
+kita ingin mencoba permen lolipop dengan 3 rasa berbeda, melon, anggur dan jeruk
+
+langkah pertama kita tentukan rasa permen apa yang akan kita coba terlebih dahulu
+pertama yang kita coba adalah permen lolipop dengan rasa anggur
+ingat, mulut kita hanya muat 1 permen
+jika kita mencoba lebih dari satu permen sekaligus, mulut kita menjadi sakit dan rasa permen sulit diidentifikasi
+
+setelah permen lolipop anggur selesai dimakan
+selanjutnya kita memakan lolipop dengan rasa yang lain sesuai pilihan
+
+ulangi cara yang sama untuk memakan lolipop
+*/
