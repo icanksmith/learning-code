4a482542ae0927dbec440bb544038634c257ff63
icank smith <sammykitkat8@gmail.com>
2024-06-30 20:52:49 +0700

    #

---
+++ part6/looping.js
@@ -0,0 +1,30 @@
+// Dalam looping kuncinya yaitu membuat nilai true ke false
+// for loop
+console.log('for loop example');
+
+// contoh for loop pertama
+for (let i = 0; i > -10; i--) {
+    console.log(i);
+} // output: 0 sampai -9
+
+// contoh for loop kedua
+for (let j = 0; j <= 5; j++) {
+    console.log(j);
+} // output: 0 sampai 5
+
+// while loop
+console.log('while loop example');
+
+let k = 0;
+while (k < 3) {
+    console.log(k);
+    k++;
+}
+
+
+// contoh indexing dalam looping
+let name = "samuel";
+
+for (l = 0; l < name.length; l++) {
+    console.log(name[l]);
+}
